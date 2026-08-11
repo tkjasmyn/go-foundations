8c8af6bd67bec32acac75295c2b972bbdc9aee51
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-08-11 10:13:17 +0100

    feat: valid parenthesis review

---
+++ algorithms/reviews/valid_parenthesis_review.go
@@ -0,0 +1,26 @@
+package reviews
+
+func ValidParentheses(s string) bool {
+	stack := []rune{}
+
+	for _, char := range s {
+		if char == '(' || char == '[' || char == '{' {
+			stack = append(stack, char)
+			continue
+		}
+
+		if len(stack) == 0 {
+			return false
+		}
+
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if (char == ')' && top != '(') ||
+		   (char == ']' && top != '[') ||
+		   (char == '}' && top != '{') {
+			return false
+		   }
+	}
+	return len(stack) == 0
+}
