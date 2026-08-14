4369960eb221f0a11239aba55386479c501afb68
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-08-14 12:53:06 +0100

    test: httptest for GET/DELETE + refactor respondJSON

--- projects/key-value-store/main.go
+++ projects/key-value-store/main.go
@@ -30,27 +30,19 @@ func (s *store) PUT(w http.ResponseWriter, r *http.Request)  {
 
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	request := request{}
 	err = json.Unmarshal(data, &request)
 	if err != nil {
-		fmt.Printf("Error:%v", err)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 	s.data[key] = request.Value
 	resp := response{Status: "stored"}
-	
-	res, err := json.MarshalIndent(resp, " ", "\n")
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
+	s.respondJSON(w, resp)
 }
 
 func (s *store) GET(w http.ResponseWriter, r *http.Request) {
@@ -61,15 +53,7 @@ func (s *store) GET(w http.ResponseWriter, r *http.Request) {
 	if _, ok := s.data[key]; ok {
 		value := s.data[key]
 		resp := request{Value: value}
-
-		res, err := json.MarshalIndent(resp, " ", "\n")
-		if err != nil {
-			fmt.Printf("Error: %v", err)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(res)
+		s.respondJSON(w, resp)
 	} else {
 		http.Error(w, "Not found", 404)
 		return
@@ -85,19 +69,22 @@ func (s *store) DELETE(w http.ResponseWriter, r *http.Request)  {
 	if _, ok := s.data[key]; ok {
 		delete(s.data, key)
 		resp := response{Status: "deleted"}
-
-		res, err := json.MarshalIndent(resp, " ", "\n")
-		if err != nil {
-			fmt.Printf("Error: %v", err)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(res)
+		s.respondJSON(w, resp)
 	} else {
 		http.Error(w, "Not found", 404)
 		return
 	}
+}
+
+func (s *store) respondJSON(w http.ResponseWriter, data any)  {
+	res, err := json.MarshalIndent(data, " ", "\n")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
 
 func (s *store) HandleKey(w http.ResponseWriter, r *http.Request)  {
@@ -116,5 +103,6 @@ func main()  {
 		data: make(map[string]string),
 	}
 	http.HandleFunc("/key/", store.HandleKey)
+	fmt.Println("Server running on port :8080")
 	http.ListenAndServe(":8080", nil)
 }
--- projects/key-value-store/main_test.go
+++ projects/key-value-store/main_test.go
@@ -21,3 +21,53 @@ func TestPUT(t *testing.T)  {
 		t.Errorf("Expected {status: stored}, got %v", rr.Body.String())
 	}
 }
+
+func TestGET(t *testing.T)  {
+	body := strings.NewReader(`{"value": "tolu"}`)
+	req := httptest.NewRequest("PUT", "/key/username", body)
+	rr := httptest.NewRecorder()
+
+	store := &store{data: make(map[string]string)}
+	store.PUT(rr, req)
+
+	req2 := httptest.NewRequest("GET", "/key/username", body)
+	rr2 := httptest.NewRecorder()
+
+	store.GET(rr2, req2)
+	if !strings.Contains(rr2.Body.String(), "tolu") {
+		t.Errorf("Expected {value: tolu, got %v", rr2.Body.String())
+	}
+
+	req3 := httptest.NewRequest("GET", "/key/nonexistent", nil)
+	rr3 := httptest.NewRecorder()
+
+	store.GET(rr3, req3)
+	if rr3.Code != 404 {
+		t.Errorf("Expected status code 404, got %v", rr3.Code)
+	}
+}
+
+func TestDELETE(t *testing.T)  {
+	body := strings.NewReader(`{"value": "tolu"}`)
+	req := httptest.NewRequest("PUT", "/key/username", body)
+	rr := httptest.NewRecorder()
+
+	store := &store{data: make(map[string]string)}
+	store.PUT(rr, req)
+
+	req2 := httptest.NewRequest("DELETE", "/key/username", body)
+	rr2 := httptest.NewRecorder()
+
+	store.DELETE(rr2, req2)
+	if !strings.Contains(rr2.Body.String(), "deleted") {
+		t.Errorf("Expected {status: deleted}, got %v", rr2.Body.String())
+	}
+
+	req3 := httptest.NewRequest("DELETE", "/key/nonexistent", nil)
+	rr3 := httptest.NewRecorder()
+
+	store.DELETE(rr3, req3)
+	if rr3.Code != 404 {
+		t.Errorf("Expected status code 404, got %v", rr3.Code)
+	}
+}
