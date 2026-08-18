995a72efd094e08f1795074ea02e51da01cee25e
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-08-18 11:32:55 +0100

    review: container with most water (max area, two pointers)

--- algorithms/reviews/max_area_review.go
+++ algorithms/reviews/max_area_review.go
@@ -3,21 +3,26 @@ package reviews
 func MaxArea(height []int) int {
 	left := 0
 	right := len(height)-1
-	maxArea := 0
+	var max int
+
+	if len(height) < 2 {
+		return -1	
+	}
 
 	for left < right {
 		width := right - left
 		h := min(height[left], height[right])
 		water := width * h
 
-		if water > maxArea {
-			maxArea = water
-		}
-		if height[left] < height[right] {
+		if height[left] <= height[right] {
 			left++
 		} else {
 			right--
 		}
+
+		if water > max {
+			max = water
+		}
 	}
-	return maxArea
+	return max
 }
