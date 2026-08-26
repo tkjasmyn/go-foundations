9ba6c284d8c837b40845c10d4af767eb521e6609
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-08-26 13:19:11 +0100

    chat-server: websocket upgrade handler + echo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-foundations
 
 go 1.26.1
+
+require github.com/gorilla/websocket v1.5.3
--- projects/chat-server/main.go
+++ projects/chat-server/main.go
@@ -2,13 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func ws(w http.ResponseWriter, r *http.Request)  {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println("Client connected")
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Printf("Received: %s\n", string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
 
 func main()  {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
+
+	http.HandleFunc("/ws", ws)
 	fmt.Println("Server running on port :8080")
 	http.ListenAndServe(":8080", nil)
 }
