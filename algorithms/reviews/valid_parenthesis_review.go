c9a9832b1857557e153d4efc9e0e1930b2ac695d
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-08-28 11:36:39 +0100

    review: valid parentheses algorithm

--- algorithms/reviews/valid_parenthesis_review.go
+++ algorithms/reviews/valid_parenthesis_review.go
@@ -4,21 +4,22 @@ func ValidParentheses(s string) bool {
 	stack := []rune{}
 
 	for _, char := range s {
-		if char == '(' || char == '[' || char == '{' {
+		if char == '(' || char == '{' || char == '[' {
 			stack = append(stack, char)
-		} else {
-			if len(stack) == 0 {
-				return false
-			}
-	
-			top := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-	
-			if (top != '(' && char == ')') || 
-			   (top != '[' && char == ']') || 
-			   (top != '{' && char == '}') {
-				return false
-			}
+			continue
+		}
+
+		if len(stack) == 0 {
+			return false
+		}
+		
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if (char == ')' && top != '(') || 
+		   (char == '}' && top != '{') || 
+		   (char == ']' && top != '[') {
+			return false
 		}
 	}
 	return len(stack) == 0
