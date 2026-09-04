55ff6ce7fb237936bd3305197a923251068b1e53
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-09-04 11:49:48 +0100

    review: product except self (BUGGY, fixed with debug trace)

--- algorithms/reviews/product_except_self.go
+++ algorithms/reviews/product_except_self.go
@@ -2,17 +2,20 @@ package reviews
 
 func ProductExceptSelf(nums []int) []int {
 	output := make([]int, len(nums))
-
-	leftproduct := 1
-	for i := 0; i < len(nums); i++ {
-		output[i] = leftproduct
-		leftproduct *= nums[i]
+	for i := range output {
+		output[i] = 1
 	}
 
-	rightproduct := 1
-	for i := len(nums) - 1; i >= 0; i-- {
-		output[i] *= rightproduct
-		rightproduct *= nums[i]
+	leftProduct := 1
+	for i := 0; i < len(nums); i++ {
+		output[i] *= leftProduct
+		leftProduct *= nums[i]
+	}
+
+	rightProduct := 1
+	for i := len(nums)-1; i >= 0; i-- {
+		output[i] *= rightProduct
+		rightProduct *= nums[i]
 	}
 	return output
 }
