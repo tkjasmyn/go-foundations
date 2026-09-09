47c0463c26576f0e6669342f95159b358536c989
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-09-09 15:37:10 +0100

    mini redis: shared map, mutex, SET/GET/DEL, input validation

--- projects/mini-redis/main.go
+++ projects/mini-redis/main.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"sync"
 )
 
 func main()  {
+	var store = make(map[string]string)
+	var mu sync.Mutex
+
+
 	ln, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -35,30 +40,60 @@ func main()  {
 				cleaned := strings.TrimSpace(str)
 				parts := strings.Fields(cleaned)
 
+				
 				if len(parts) == 0 {
 					c.Write([]byte("Empty Command." + "\n"))
 					continue
 				}
-
+				
+				
 				switch parts[0] {
 				case "SET":
 					if len(parts) < 3 {
 						c.Write([]byte("Invalid Input" + "\n"))
 						continue
 					}
-					c.Write([]byte("got SET key=" + parts[1] + " value=" + parts[2] + "\n"))
+
+					key := parts[1]
+					value := parts[2]
+
+					mu.Lock()
+						store[key] = value
+					mu.Unlock()
+
+					c.Write([]byte("+OK" + "\n"))
 				case "GET":
 					if len(parts) < 2 {
 						c.Write([]byte("Invalid Input" + "\n"))
 						continue
 					}
-					c.Write([]byte("got GET key=" + parts[1] + "\n"))
+
+					key := parts[1]
+					
+					mu.Lock()
+					if value, ok := store[key]; ok {
+						c.Write([]byte(value + "\n"))
+						} else {
+							c.Write([]byte("(nil)" + "\n"))
+						}
+					mu.Unlock()
 				case "DEL":
 					if len(parts) < 2 {
 						c.Write([]byte("Invalid Input" + "\n"))
 						continue
 					}
-					c.Write([]byte("got DEL key=" + parts[1] + "\n"))
+					
+					key := parts[1]
+
+					mu.Lock()
+					_, ok := store[key]
+					if ok {
+						delete(store, key)
+						c.Write([]byte(":1" + "\n"))
+					} else {
+						c.Write([]byte(":0" + "\n"))
+					}
+					mu.Unlock()
 				default:
 					c.Write([]byte("Unrecognized Command." + "\n"))
 				}
