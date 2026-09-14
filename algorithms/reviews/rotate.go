d9069c7940bbde370f4214e35cbb44adc79b9618
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-09-14 14:44:22 +0100

    review: rotate algorithm

---
+++ algorithms/reviews/rotate.go
@@ -0,0 +1,15 @@
+package reviews
+
+func Rotate(nums []int, k int) {
+	if len(nums) == 0 {
+		return
+	}
+
+	res := make([]int, len(nums))
+	k = k % len(nums)
+
+	for i := 0; i < len(nums); i++ {
+		res[(i+k) % len(nums)] = nums[i]
+	}
+	copy(nums, res)
+}
